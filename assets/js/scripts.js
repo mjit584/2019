a5bd04963bbca0c4684331408ea7cf00b89113ba
Brooke Fisher <brookenfish1@gmail.com>
2019-02-05 18:13:03 -0500

    Changed background image, added an _index.md file and pulled from its content to add in information on the homepage; Added in some circles for about page image mock-ups

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,28 +2,7 @@ $(function () {
   console.log('Hello world');
 });
 
-//variables
 
-/**
- * Activating the website navigation dots according to the given slide name.
- */
-function activateNavDots(name, sectionIndex) {
-  if (options.navigation) {
-    $('#nav-home').find('.active').removeClass('active');
-    if (name) {
-      $('#nav-home').find('a[href="#' + name + '"]').addClass('active');
-    } else {
-      $('#nav-home').find('li').eq(sectionIndex).find('a').addClass('active');
-    }
-  }
-}
+/**write conditional logic statement that adds the class .active to a span if the parent a href's matches the section's id, else, remove the class .active  */
 
-/**
- * Activating the website main menu elements according to the given slide name.
- */
-function activateMenuElement(name) {
-  if (options.menu) {
-    $(options.menu).find('.active').removeClass('active');
-    $(options.menu).find('[data-menuanchor="' + name + '"]').addClass('active');
-  }
-}
+/** write a function when on hover of a span, there will be a tooltip that reads out the data-tooltip */
