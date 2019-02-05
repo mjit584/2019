4554dbfe281489bb8a70ef26f30e0ae93861cfd7
Brooke Fisher <brookenfish1@gmail.com>
2019-02-05 17:41:10 -0500

    fiddled with adding in active state depending on where the user is on the page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,29 @@
 $(function () {
   console.log('Hello world');
 });
+
+//variables
+
+/**
+ * Activating the website navigation dots according to the given slide name.
+ */
+function activateNavDots(name, sectionIndex) {
+  if (options.navigation) {
+    $('#nav-home').find('.active').removeClass('active');
+    if (name) {
+      $('#nav-home').find('a[href="#' + name + '"]').addClass('active');
+    } else {
+      $('#nav-home').find('li').eq(sectionIndex).find('a').addClass('active');
+    }
+  }
+}
+
+/**
+ * Activating the website main menu elements according to the given slide name.
+ */
+function activateMenuElement(name) {
+  if (options.menu) {
+    $(options.menu).find('.active').removeClass('active');
+    $(options.menu).find('[data-menuanchor="' + name + '"]').addClass('active');
+  }
+}
