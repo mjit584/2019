f80d21ee87a608ea9461d4343a12c7dc93228ecd
Amy Townsend <amygtownsend@gmail.com>
2019-02-05 16:04:23 -0500

    add feature for hiding header on scroll down

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,14 @@
 $(function () {
   console.log('Hello world');
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
