675b20c39f74af055a030c798536ca2129406b26
Brooke Fisher <brookenfish1@gmail.com>
2019-02-06 17:31:01 -0500

    Fixed scripts .concat problem and added a link for the interactive page

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,5 @@
+console.log('testing to make sure headers.js is linked up');
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
