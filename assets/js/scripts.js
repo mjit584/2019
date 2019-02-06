21e6e41502ec8573d517fe7fc8d222672b9e2c07
Brooke Fisher <brookenfish1@gmail.com>
2019-02-06 13:11:25 -0500

    Created an asset bundler to take all js scripts and put them into one file called bundle.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,16 +5,3 @@ $(function () {
 /**write conditional logic statement that adds the class .active to a span if the parent a href's matches the section's id, else, remove the class .active  */
 
 /** write a function when on hover of a span, there will be a tooltip that reads out the data-tooltip */
-
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-75px";
-  }
-  prevScrollpos = currentScrollPos;
-}
