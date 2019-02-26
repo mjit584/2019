30a0235146a7f977c8affde398df438976324630
Brooke Fisher <brookenfish1@gmail.com>
2019-02-26 14:45:11 -0500

    Created a partial for the about js info and added in mixitup js

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,48 +3,53 @@
 console.log('Testing to see if about is loaded');
 
 // Define about variables
-var everyone = document.getElementById('about__filter--everyone');
-var photog = document.getElementById('about__filter--photog');
-var vid = document.getElementById('about__filter--vid');
-var rep = document.getElementById('about__filter--rep');
-var des = document.getElementById('about__filter--des');
-var dev = document.getElementById('about__filter--dev');
-var coaches = document.getElementById('about__filter--coach');
+var filterEveryone = document.getElementById('about__filter--everyone');
+var filterPhotog = document.getElementById('about__filter--photog');
+var filterVid = document.getElementById('about__filter--vid');
+var filterRep = document.getElementById('about__filter--rep');
+var filterDes = document.getElementById('about__filter--des');
+var filterDev = document.getElementById('about__filter--dev');
+var filterCoaches = document.getElementById('about__filter--coach');
+var all = document.getElementById('all');
+var photog = document.getElementById('photog')
 
-
-photog.onclick = function () {
-  if (everyone.classList.contains('is-active')) {
-    photog.classList.toggle('is-active');
-    everyone.classList.toggle('is-active');
-  } else {
-    everyone.classList.toggle('is-active');
-    photog.classList.toggle('is-active');
-  }
+filterEveryone.onclick = function () {
+  all.classList.remove('hidden');
+  photog.classList.add('hidden');
+  filterPhotog.classList.remove('is-active');
+  filterEveryone.classList.add('is-active');
 }
 
-vid.onclick = function () {
-  vid.classList.toggle('is-active');
-  everyone.classList.toggle('is-active');
+filterPhotog.onclick = function () {
+  photog.classList.toggle('hidden');
+  all.classList.toggle('hidden');
+  filterEveryone.classList.toggle('is-active');
+  filterPhotog.classList.toggle('is-active');
 }
 
-rep.onclick = function () {
-  rep.classList.toggle('is-active');
-  everyone.classList.toggle('is-active');
+filterVid.onclick = function () {
+  filterVid.classList.toggle('is-active');
+  filterEveryone.classList.toggle('is-active');
 }
 
-des.onclick = function () {
-  des.classList.toggle('is-active');
-  everyone.classList.toggle('is-active');
+filterRep.onclick = function () {
+  filterRep.classList.toggle('is-active');
+  filterEveryone.classList.toggle('is-active');
 }
 
-dev.onclick = function () {
-  dev.classList.toggle('is-active');
-  everyone.classList.toggle('is-active');
+filterDes.onclick = function () {
+  filterDes.classList.toggle('is-active');
+  filterEveryone.classList.toggle('is-active');
 }
 
-coaches.onclick = function () {
-  coaches.classList.toggle('is-active');
-  everyone.classList.toggle('is-active');
+filterDev.onclick = function () {
+  filterDev.classList.toggle('is-active');
+  filterEveryone.classList.toggle('is-active');
+}
+
+filterCoaches.onclick = function () {
+  filterCoaches.classList.toggle('is-active');
+  filterEveryone.classList.toggle('is-active');
 }
 
 
