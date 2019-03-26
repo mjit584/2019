056e3a96e6a08c9238e68fa62522fb4529564a9b
Amy Townsend <amygtownsend@gmail.com>
2019-03-26 00:18:31 -0400

    Add section 3 content, fix scroll magic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -17,16 +17,57 @@ let init = function () {
   // get all slides
   let slides = document.querySelectorAll("section.panel");
 
+  new ScrollMagic.Scene({
+    triggerElement: slides[0],
+    duration: 1
+  })
+    .setPin(slides[0])
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: slides[1],
+    duration: 300
+  })
+    .setPin(slides[1])
+    .addTo(controller);
+
   // create a scene for every slide
-  slides.forEach((slide) => {
-    new ScrollMagic.Scene({
-      triggerElement: slide
-    })
-      .setPin(slide)
-      .addTo(controller);
-  });
+  // slides.forEach((slide) => {
+  //   let h = slide.clientHeight;
+  //   console.log(slide.clientHeight);
+  //   durat.push(h);
+  // });
+  // slides.forEach((slide, index) => {
+  //   console.log(durat[index]);
+  //   new ScrollMagic.Scene({
+  //     triggerElement: slide,
+  //     duration: durat[index]
+  //   })
+  //     .setPin(slide, {
+  //       pushFollowers: false
+  //     })
+  //     .addTo(controller);
+  // });
 
 }
+
+// if (index === 2) {
+//   new ScrollMagic.Scene({
+//     triggerElement: slide.get(index),
+//     duration: durat[index]
+//   })
+//     .setPin(slide.get(index), {
+//       pushFollowers: false
+//     })
+//     .addTo(controller);
+// } else {
+//   new ScrollMagic.Scene({
+//     triggerElement: slide,
+//   })
+//     .setPin(slide)
+//     .addTo(controller);
+// }
+
 
 document.addEventListener("DOMContentLoaded", init);
 
