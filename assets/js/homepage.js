7683789fc51a861fdd871cffa342731729c25bb3
Amy Townsend <amygtownsend@gmail.com>
2019-04-17 21:12:08 -0400

    Remove homepage video lights out effect

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -99,46 +99,6 @@ let init = function () {
       }
     }
   });
-
-  // Home about video go dark settings
-  let videoTop;
-  let videoHeight;
-  let windowWidth;
-  windowWidth = $(window).width();
-  videoTop = $('#video-top').offset().top;
-  videoHeight = $('iframe').outerHeight();
-  $(window).scroll(function (event) {
-    if (windowWidth > 1240) {
-      goDark();
-    }
-  })
-
-  $(window).resize(function () {
-    windowWidth = $(window).width();
-    videoTop = $('#video-top').offset().top;
-    videoHeight = $('iframe').outerHeight();
-    if (windowWidth > 1240) {
-      goDark();
-    } else {
-      $('.home').css('background-color', '#fff')
-      $('p, h1, h2, h3, h4, h5, h6, footer, .about-help').css('opacity', '1')
-    }
-  });
-
-  function goDark() {
-    var scrollTop = $(this).scrollTop();
-    // console.log(scrollTop)
-    if (scrollTop >= videoTop - (videoHeight / 2) && scrollTop <= videoTop + (videoHeight / 10)) {
-      $('#home__about').css('background-color', '#000')
-      $('p, h1, h2, h3, h4, h5, h6, footer, .about-help').css('opacity', '0')
-    } else if (scrollTop > videoTop + (videoHeight / 10)) {
-      $('#home__about').css('background-color', '#fff')
-      $('p, h1, h2, h3, h4, h5, h6, footer, .about-help').css('opacity', '1')
-    } else if (scrollTop < videoTop - (videoHeight / 2)) {
-      $('#home__about').css('background-color', '#fff')
-      $('p, h1, h2, h3, h4, h5, h6, footer, .about-help').css('opacity', '1')
-    };
-  };
 }
 
 document.addEventListener("DOMContentLoaded", init);
